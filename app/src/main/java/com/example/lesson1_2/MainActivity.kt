4fddbca0608098a00612465e5d148201b88a6592
Ilya Chernetsov <dev.i.chernetsov@gmail.com>
2021-11-03 17:39:10 +0300

    Initial

---
+++ app/src/main/java/com/example/lesson1_2/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.lesson1_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun showText(view: View){
+        Toast.makeText(this, "Text Created", Toast.LENGTH_SHORT).show()
+    }
+
+    fun creatingRandomNumbers(view: View){
+        var counter:Int = (0..50).random()
+        val setter:TextView = findViewById(R.id.text_Number)
+        setter.text = counter.toString()
+    }
+}
